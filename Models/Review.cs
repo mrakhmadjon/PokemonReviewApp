213e68808587b045a7cdba45ac640e74886ec3b3
Akhmadjon <mrjohnaccaount@gmail.com>
2023-12-18 23:37:21 +0100

    Review model has one to one relation with Pokemon

--- Models/Review.cs
+++ Models/Review.cs
@@ -6,5 +6,6 @@
         public string Text { get; set; }
         public string Title { get; set; }
         public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }

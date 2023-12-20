a53b4a74c8029ff9aaf1a2597d58a2ff496b245a
Akhmadjon <mrjohnaccaount@gmail.com>
2023-12-20 23:57:58 +0100

    PokemonDbContext added

---
+++ Data/PokemonDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class PokemonDbContext : DbContext
+    {
+        public PokemonDbContext(DbContextOptions<PokemonDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Country> country { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
